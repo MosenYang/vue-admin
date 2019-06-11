0ece008f37c9261652a585a4598ca51631f5684f
mosen <yangmengsheng@163.com>
2019-06-11 15:43:30 +0800

    兼容请求方法包含 .mock 和线上接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,21 +2,28 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+
+const defaultSettings = require('@/settings.js')
+
 // 索邦接口
 // url:"http://api.thisyang.online/api/products"
 // headers: {
 //   Accept: "application/json",
 //   Authorization:"Bearer... "
 // }
-// console.log('vue环境',process.env.VUE_APP_BASE_API)
-// console.log('索邦环境', process.env.SUO_BANG_BASE_API)
+
+console.log('vue环境api', process.env.VUE_APP_BASE_API)
+// console.log('索邦环境api', process.env.SUO_BANG_BASE_API)
+// console.log('索邦环境url', process.env.SUO_BANG_BASE_URL)
+
 const service = axios.create({
+  // 为什么没有.因为我需要切换mock和线上地址, 把地址分成两段了,完整url在代理里面拼接了,中间这段就省去
   baseURL: process.env.VUE_APP_BASE_API,
   withCredentials: true, // 跨域发送请求
   timeout: 8000,
   headers: {
     Accept: 'application/json',
-    SUOBANG: process.env.SUO_BANG_BASE_API
+    Authorization: localStorage.getItem('token') ? localStorage.getItem('token') : ''
   }
 })
 
@@ -34,14 +41,22 @@ service.interceptors.request.use(config => {
 )
 
 // 响应拦截器
+// 兼容mock 和线上接口200/400状态 (Mosen 添加)
 service.interceptors.response.use(response => {
     const res = response.data
-    if (res.code !== 20000) {
+    if (res.code !== 20000 && res.code !== 200) {
       Message({
         message: res.message || '错误',
         type: 'error',
         duration: 5 * 1000
       })
+      if (res.code === 400) {
+        Message({
+          message: '接口400了',
+          type: 'warning',
+          duration: 1000
+        })
+      }
       // 50008: 无效token; 50012: 其他客户端登录; 50014: 登录超时;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         MessageBox.confirm('你可以取消,也可以重新登录!', 'Confirm logout', {
@@ -57,6 +72,11 @@ service.interceptors.response.use(response => {
       }
       return Promise.reject(res.message || 'error')
     } else {
+      Message({
+        message: '接口成功',
+        type: 'success',
+        duration: 1000
+      })
       return res
     }
   },
